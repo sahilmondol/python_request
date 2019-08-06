4220582aaa810c4ee1c45073398e9c043c9c5e90
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-06 12:42:24 +0530

    miscelleneous changes

--- actions/requests_post.py
+++ actions/requests_post.py
@@ -1,14 +1,15 @@
 import requests
 import sys
 import json
-import datetime
+from json import dumps
+from datetime import datetime
 
 from st2common.runners.base_action import Action
 
 class requests_api(Action):
 	def run(self,ID,Title,Description,PageCount,Excerpt):
 		url = 'https://api.github.com/some/endpoint'
-		time = datetime.datetime.now()
+		time = str(datetime.utcnow())
 		payload = {'ID': ID,
 					'Title': Title,
 					'Description': Description,
