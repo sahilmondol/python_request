d1f15f3f785e3136b1d805bf97e557c9ad006396
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-06 12:09:06 +0530

    miscelleneous changes

--- actions/requests_post.py
+++ actions/requests_post.py
@@ -8,11 +8,12 @@ from st2common.runners.base_action import Action
 class requests_api(Action):
 	def run(self,ID,Title,Description,PageCount,Excerpt):
 		url = 'https://api.github.com/some/endpoint'
+		time = datetime.datetime.now()
 		payload = {'ID': ID,
 					'Title': Title,
 					'Description': Description,
 					'PageCount': PageCount,
 					'Excerpt': Excerpt,
-					'PublishDate':datetime.datetime.now()}
+					'PublishDate':time}
 		r = requests.post(url, data=json.dumps(payload))
 		print(r)
