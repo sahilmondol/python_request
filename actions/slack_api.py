2e7b476966db35fde26ab4cb609a47fe69cfcced
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-13 11:43:57 +0530

    miscellaneous changes

---
+++ actions/slack_api.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+import json
+from json import dumps
+from datetime import datetime
+
+from st2common.runners.base_action import Action
+
+class requests_api(Action):
+	def run(self, url):
+		time = str(datetime.utcnow())
+		payload = {"channel": "xoxp-569934227830-706838758277-708758560834-9f695114116fbb7339f5e4da466fdf0c",
+  					"text": "Hello, from StackStorm!!!"}
+		header = {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+		}
+		data=json.dumps(payload)
+		r = requests.post(url,data,headers = header)
+		result = r.json()
+		print(result)
