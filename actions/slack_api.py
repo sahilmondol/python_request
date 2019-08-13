aac975de812f01b45263ce31c243dad17d76aeb9
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-13 14:35:29 +0530

    miscellaneous changes

--- actions/slack_api.py
+++ actions/slack_api.py
@@ -9,11 +9,11 @@ from st2common.runners.base_action import Action
 class slack_api(Action):
 	def run(self):
 		url = "https://slack.com/api/chat.postMessage"
-		payload = {"channel": "CLSA44LA0",
+		payload = {"channel": "CGQB47LKV",
   					"text": "Hello, from StackStorm!!!"}
 		header = {'Content-Type': 'application/json',
 			'Accept': 'application/json',
-        	'Authorization' :'Bearer xoxp-569934227830-706838758277-713216048418-17cacc26cf397d395dd6122ef41dd2a9'
+        	'Authorization' :'Bearer xoxp-569934227830-706838758277-724192098436-0a37281e9e840fe25677eb91f7d8f029'
         	}
 		data=json.dumps(payload)
 		r = requests.post(url,data,headers = header)
