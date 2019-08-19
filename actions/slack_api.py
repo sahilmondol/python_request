d831cab9958f2cbc1fde1774088dc2ef5907c7ce
Sahil Mondol <sahilmondol95@gmail.com>
2019-08-19 13:25:18 +0530

    miscellaneous changes

--- actions/slack_api.py
+++ actions/slack_api.py
@@ -6,14 +6,14 @@ from datetime import datetime
 from st2common.runners.base_action import Action
 
 class slack_api(Action):
-	def run(self,token,message):
+	def run(self,message):
 		url = "https://slack.com/api/chat.postMessage"
 		payload = {"channel": "CGQB47LKV",
   					"text": message
   					}
 		header = {'Content-Type': 'application/json',
 			'Accept': 'application/json',
-        	'Authorization' : token
+        	'access_token' : 'xoxp-569934227830-706838758277-730251995415-73ec11d4ebd26e275e64e32a2de92a3b'
         	}
 		data=json.dumps(payload)
 		r = requests.post(url,data,headers = header)
